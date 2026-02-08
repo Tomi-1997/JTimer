c87d5f0fd123563be64d595910e57a516a339df4
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2026-02-08 11:44:32 +0200

    move listenforinput

--- src/utils/Helpers.java
+++ src/utils/Helpers.java
@@ -1,6 +1,15 @@
 package utils;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Helpers {
+
+    public enum SessionType {
+        Undefined,
+        Normal, // Single timer, modifiable
+        plan // series of timers in a planned schedule
+    }
 
     public static boolean isNumber(String flag) {
         try {
@@ -18,4 +27,21 @@ public class Helpers {
         }
         System.out.println();
     }
+
+    public static void listenForInput(Scanner sc, Timer timer, SessionType currSession) throws IOException {
+        String userInput = sc.nextLine();
+        boolean isInputNeeded = (currSession == SessionType.Normal);
+        while (!userInput.trim().isEmpty() && isInputNeeded) {
+            if (isNumber(userInput)) {
+                timer.setMinutes(Integer.parseInt(userInput));
+                System.out.println("The timer is now set at " + timer.getMinutes() + " min");
+                if (timer.getMinutes() > 60) {
+                    System.out.println("--WARNING: the session is over an hour");
+                }
+                System.out.println("Press enter to continue");
+            }
+            userInput = sc.nextLine();
+        }
+    }
+
 }
