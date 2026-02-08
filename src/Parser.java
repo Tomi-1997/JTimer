b87fa70af04d4074fb72f3f6af48aa132683679c
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2026-02-08 11:45:42 +0200

    + update flag info + generalise parseCommands to parse flags too

--- src/Parser.java
+++ src/Parser.java
@@ -14,11 +14,11 @@ public class Parser {
 
     private void initFlags() {
         flagMap.put("l", new Flag("lower", "Lower volume"));
-        flagMap.put("L", new Flag("lower lower", "Much lower volume (only as argument)"));
+        flagMap.put("L", new Flag("Lower", "Much lower volume (only as argument)"));
         flagMap.put("u", new Flag("undo", "Undo volume change (only on info screen)"));
-        flagMap.put("t", new Flag("test", "Test current volume"));
+        flagMap.put("t", new Flag("test", "Test current volume (only on info screen)"));
         flagMap.put("n", new Flag("notify", "Disable/Enable notification when timer ends (Enabled on default)"));
-        flagMap.put("p", new Flag("plan", "Start a planned session with given JSON file"));
+        flagMap.put("p", new Flag("plan", "Start a planned session with given JSON file (-p <path to json>) "));
     }
 
     public String toString() {
@@ -32,18 +32,27 @@ public class Parser {
         return builder.toString();
     }
 
-    public String parseCommand(String cmd) {
-        if (cmd == null || cmd.isEmpty()) {
+    public String parseArgs(String arg, boolean isFlag) {
+        if (arg == null || arg.strip().isEmpty()) {
             return null;
         }
-        if(isNumber(cmd)) return "#";
-
-        String key = cmd.substring(0, 1);
+        if(isNumber(arg)) return "#";
+        if(isFlag){
+            // strip '-'
+            if(arg.charAt(0)!= '-'){
+                return null;
+            }
+            if (arg.length() != 2){
+                return null;
+            }
+            arg = arg.replaceFirst("-", "");
+        }
+        String key = arg.substring(0, 1);
         if (flagMap.containsKey(key)) {
-            if (cmd.length() == 1)
+            if (arg.length() == 1)
                 return key;
             Flag value = flagMap.get(key);
-            if (value.longArg.equals(cmd)) {
+            if (value.longArg.equals(arg)) {
                 return key;
             }
         }
